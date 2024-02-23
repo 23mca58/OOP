a3cdeb3c5913920b1edad01ab0003dbe93a45d0d
23mca58 <148203368+23mca58@users.noreply.github.com>
2024-02-23 15:55:27 +0530

    Add files via upload

---
+++ S2q2.java
@@ -0,0 +1,14 @@
+class S2q2
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c=a+b;
+int s=a-b;
+int m=a*b;
+int d=a/b;
+int e=a%b;
+System.out.println("Addition = "+c+"\nSubtraction = "+s+"\nMultiplication = "+m+"\nDivision = "+d+"\nModulus = "+e);
+}
+}
